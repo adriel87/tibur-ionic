93772f880e594bff45ae3489e89d11e3cada4cb5
adriel <adrielarochaoronoz@gmail.com>
2022-09-17 14:52:11 +0100

    adding cors

--- backend_tibur/index.js
+++ backend_tibur/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
+const cors = require('cors')
 
 const app = express();
 // aplicamos diferentes middlewares
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 //creamos nuestra primera ruta
 
